b67f389a7bf36ab9130d2a47d936960e5beac691
Alphajaywarrior <143625566+Alphajaywarrior@users.noreply.github.com>
2024-02-21 15:47:18 +0000

    I made a paragraph

--- script.js
+++ script.js
@@ -5,11 +5,14 @@ buttonOne.addEventListener("click", function() {
     // 1. Declare a variable named spidyCaption.
     //  - Store the value of the input with a class of "comment-one".
 
-
+  let spidycap = document.querySelector(".comment-one").value;
     // 2. Display the variable spidyCaption to captionOne.
+  captionOne.innerHTML = spidycap;
 
 
-});
+});;
 
 // 3. Repeat the previous steps for the second meme.
 //  - Be sure to store variables for the button and caption.
+let frogCaption = document.querySelector(".caption-two");
+captionTwo.innerHTML = frogCaption;
